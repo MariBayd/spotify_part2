0df6aad925c1e8f6deb0ca0fbbef000977453c7f
MariBayd <masha.baydina@gmail.com>
2022-05-30 12:35:42 +0300

    replacing innerHtml with replaceChildren

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -7,6 +7,7 @@ import Artist from './Artist.js';
     const clientId = 'd0b55c9378d0473eb89d96c3b2e1a01c';
     const clientSecret = '00f9a24f25254ee4a3313f66fdbb0968';
 
+
     /** Get data */ 
     const apiController = new APIController(clientId, clientSecret);
     const token = await apiController.getToken();
@@ -24,7 +25,7 @@ import Artist from './Artist.js';
             .getData(apiController.urlSearch + inputSearch.value + '&type=artist', 0, token);
         
         if (searchResults.artists.items.length) {
-            document.getElementById('results').innerHTML="";
+            document.getElementById('results').replaceChildren();
             searchResults.artists.items.forEach(item => new Artist(item, 'results'));
         } else {
             setNoResult();
@@ -35,7 +36,7 @@ import Artist from './Artist.js';
      *  Insert message in html, if items by query not found 
      */
     function setNoResult(){
-        document.getElementById('results').innerHTML="";
+        document.getElementById('results').replaceChildren();
         const noResultText = `<span id="no-result-text">По вашему запросу ничего не найдено</span>`;
         document.getElementById('results').insertAdjacentHTML('beforeend', noResultText);
     }
