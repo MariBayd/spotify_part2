178306824690e81ecd81a8d3b36f23beaf5f6bc9
MariBayd <masha.baydina@gmail.com>
2022-06-09 00:06:04 +0300

    Диплом. React

---
+++ src/Components_react/UI/Content/Content.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./Content.module.css"
+
+const Content = () => {
+    
+    return (
+        <div className={classes.content} id="content"></div>
+    )
+}
+
+export default Content;
